2ff03a422ce8bfde84e803ed1b9378dc3cee4376
Kamalc <kimo201224@gmail.com>
2021-04-28 03:43:48 +0200

    add first Project

---
+++ VS Projects/Fundamentals but vs 2019/Fundamentals but vs 2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentals_but_vs_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
